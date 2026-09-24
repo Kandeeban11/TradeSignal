08b4e550d12c32d85c5b6b94f3a69053bc0b6f76
Kandeeban11 <kandeeban941@gmail.com>
2026-09-24 16:22:21 +0530

    v1.2
    v1.2

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // Public Supabase settings. Replace both values before deployment.
 window.TRADESIGNAL_SUPABASE = {
   url: 'https://wieqokrynhlyuohtkmfj.supabase.co',
-  anonKey: 'sb_publishable_Niy8Pxqjv4GyKtBQE3G3Rg_sfuBsRFLeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpZXFva3J5bmhseXVvaHRrbWZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyMzk1NTAsImV4cCI6MjEwNTgxNTU1MH0.ZXY8pXY-0CSRrgcjVW_Odex7LTrw6X4E3XSgY50oUo0'
+  anonKey: 'sb_publishable_Niy8Pxqjv4GyKtBQE3G3Rg_sfuBsRFL'
 };
